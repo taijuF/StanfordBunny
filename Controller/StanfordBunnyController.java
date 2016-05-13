207397e1fb85e39e73f57b77fcd6f691e8b7cc5a
taijuF <taiju4451@gmail.com>
2016-05-13 13:54:24 +0900

    file

---
+++ Controller/StanfordBunnyController.java
@@ -0,0 +1,17 @@
+/*
+  controller
+*/
+package bunny;
+
+import com.jogamp.*;
+import java.util.*;
+
+class SranfordBunnyController {
+    private StanfordBunnyView view;
+    private StanfordBunnyModel model;
+    public StanfordBunnyController( StanfordBunnyView view,StanfordBunnyModel model){
+	this.view = view;
+	this.model = model;
+    }
+
+}
